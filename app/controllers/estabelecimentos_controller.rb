b0e253abdc96160ee4f5d002458b0280e437d00e
Daniel Fonseca da Silva <developer-web@programmer.net>
2022-11-04 00:48:00 -0300

    feat: Created the controller with rails generate controller inside Estabelecimentos.

---
+++ app/controllers/estabelecimentos_controller.rb
@@ -0,0 +1,5 @@
+class EstabelecimentosController < ApplicationController
+
+    
+
+end
---
+++ test/controllers/estabelecimentos_controller_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class EstabelecimentosControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
