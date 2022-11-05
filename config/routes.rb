b9c598c97af455d75182522f98c74cd29fb8ba7a
Daniel Fonseca da Silva <developer-web@programmer.net>
2022-11-05 00:26:10 -0300

    feat: Created routes to usuario comum and usuario corporativo.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  get "estabelecimentos", to: "estabelecimentos#index" 
+  get "estabelecimentos", to: "estabelecimentos#index"
+  get "usuario-comum", to: "usuario_comum#index"
+  get "usuario_corporativo", to: "usuario_corporativo#index"
 end
