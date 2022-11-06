6475fe25f8b25b2aad71ede80c4cbf12aab560de
Daniel Fonseca da Silva <developer-web@programmer.net>
2022-11-06 15:33:04 -0300

    feat: Create table with rails generate model Estabelecimentos nome:string descricao:text categoria:string numero:integer estado:string bairo:string cidade:string cep:string telefone:string site:string emai:string and rails db:migrate.

---
+++ db/migrate/20221106180938_create_estabelecimentos.rb
@@ -0,0 +1,19 @@
+class CreateEstabelecimentos < ActiveRecord::Migration[7.0]
+  def change
+    create_table :estabelecimentos do |t|
+      t.string :nome
+      t.text :descricao
+      t.string :categoria
+      t.integer :numero
+      t.string :estado
+      t.string :bairo
+      t.string :cidade
+      t.string :cep
+      t.string :telefone
+      t.string :site
+      t.string :emai
+
+      t.timestamps
+    end
+  end
+end
