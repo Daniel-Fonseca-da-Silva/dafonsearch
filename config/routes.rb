2a655d87db78803a7ad9cd64ca110853e661a602
Daniel Fonseca da Silva <developer-web@programmer.net>
2022-11-13 15:13:07 -0300

    feat: Created the route for home.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root to: "estabelecimentos#index"
+  root to: "home#home"
   get "usuario-comum", to: "usuario_comum#index"
   get "usuario_corporativo", to: "usuario_corporativo#index"
 end
