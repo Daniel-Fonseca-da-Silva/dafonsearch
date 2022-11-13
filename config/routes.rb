cbbacb111b19a4e4f17630612d0d841769a062f8
Daniel Fonseca da Silva <developer-web@programmer.net>
2022-11-13 11:08:22 -0300

    Revert "refactor: Changed the home path and created a new router for estabelecimentos."
    This reverts commit 66fa940741e5451995ef45ef5dcc5fc50ed9c396.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  root to: "home#index"
-  get "estabelecimentos", to: "estabelecimentos#index"
+  root to: "estabelecimentos#index"
   get "usuario-comum", to: "usuario_comum#index"
   get "usuario_corporativo", to: "usuario_corporativo#index"
 end
