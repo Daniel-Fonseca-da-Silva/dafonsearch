66fa940741e5451995ef45ef5dcc5fc50ed9c396
Daniel Fonseca da Silva <developer-web@programmer.net>
2022-11-13 10:12:53 -0300

    refactor: Changed the home path and created a new router for estabelecimentos.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  root to: "estabelecimentos#index"
+  root to: "home#index"
+  get "estabelecimentos", to: "estabelecimentos#index"
   get "usuario-comum", to: "usuario_comum#index"
   get "usuario_corporativo", to: "usuario_corporativo#index"
 end
